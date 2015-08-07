0134351fa7ed567ff27356f7fce6be51d25ab922
bountytheef <ncstech07@gmail.com>
2015-08-07 13:44:54 -0400

    version 1.0

---
+++ css/bootstrap-3.3.4-dist/js/_vti_cnf/bootstrap.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|16 Mar 2015 12:42:10 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|GUELOREMANUEL\\Guelor
+vti_modifiedby:SR|GUELOREMANUEL\\Guelor
+vti_timecreated:TR|16 Mar 2015 12:42:10 -0000
+vti_cacheddtm:TX|16 Mar 2015 12:42:10 -0000
+vti_filesize:IR|67546
+vti_backlinkinfo:VX|
